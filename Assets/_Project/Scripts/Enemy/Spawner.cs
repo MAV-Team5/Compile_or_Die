10b82e447c47d832b1800495a63068ff2fd26258
Oreo-Hyeon <gusrygusry513@gmail.com>
2026-04-04 22:01:55 +0900

    [Add] 몹 생성 및 풀링 플레이어와 연결

--- Assets/_Project/Scripts/Enemy/Spawner.cs
+++ Assets/_Project/Scripts/Enemy/Spawner.cs
@@ -24,7 +24,7 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,2));
+        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,1));
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
 
     }
--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public Player player;
     public PoolManager pool;
     public void Awake()
     {
