b174973cf16eaa2edbdf63da35abdf58444016d2
subin-software <scyopop.com1105@gmail.com>
2026-04-04 22:32:44 +0900

    배경 작업, 카메라

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -1,8 +1,14 @@
+<<<<<<< Updated upstream
+=======
+using System.Collections;
+using System.Collections.Generic;
+>>>>>>> Stashed changes
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+<<<<<<< Updated upstream
 
     public Player player;
     public PoolManager pool;
@@ -10,4 +16,26 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
     }
+=======
+    public Player player;
+    //public PoolManager pool;
+
+    //public float gameTime;
+    //public float maxGameTime = 2 * 10f;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    /*void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
+    }*/
+>>>>>>> Stashed changes
 }
