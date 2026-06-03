8da09dc4cdc4c6976a421213ea3451ca2feefc52
Oreo-Hyeon <gusrygusry513@gmail.com>
2026-06-03 22:24:04 +0900

    [add] 총알 적 풀링 분리. etc

--- Assets/_Project/Scripts/Enemy/PoolManager.cs
+++ Assets/_Project/Scripts/Enemy/PoolManager.cs
@@ -1,25 +1,79 @@
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-
+public enum PoolType
+{
+    Enemy,
+    Bullet,
+    Effect
+}
+//오브젝트 풀링을 위한 풀 매니저, 각각의 풀 리스트를 사용.
 public class PoolManager : MonoBehaviour
 {
+    [Header("Enemy")]
     public GameObject[] enemyPrefabs;
+    [Header("Bullet")]
+    public GameObject[] bulletPrefabs;
+    [Header("Effect")]
+    public GameObject[] effectPrefabs;
 
-    List<GameObject>[] pools;
+
+    List<GameObject>[] enemyPools;
+    List<GameObject>[] bulletPools;
+    List<GameObject>[] effectPools;
 
     void Awake()
     {
-        pools = new List<GameObject>[enemyPrefabs.Length];
+        enemyPools = CreatePools(effectPrefabs.Length);
+        bulletPools = CreatePools(bulletPrefabs.Length);
+        effectPools = CreatePools(effectPrefabs.Length);
+    }
 
-        for (int index = 0; index < pools.Length; index++)
+    /// <summary>
+    /// 각 풀 초기화 함수.
+    /// </summary>
+    /// <param name="size"></param>
+    /// <returns></returns>
+    List<GameObject>[] CreatePools(int size)
+    {
+        List<GameObject>[] pools = new List<GameObject>[size];
+
+        for (int i = 0; i < size; i++)
         {
-            pools[index] = new List<GameObject>();
+            pools[i] = new List<GameObject>();
+        }
+
+        return pools;
+    }
+    /// <summary>
+    /// 각 풀링 타입에 맞춰 오브젝트 찾아오기. 호출용.
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public GameObject Get(PoolType type, int index)
+    {
+        switch (type)
+        {
+            case PoolType.Enemy:
+                return GetObject(enemyPools, enemyPrefabs, index);
+            case PoolType.Bullet:
+                return GetObject(bulletPools, bulletPrefabs, index);
+            case PoolType.Effect:
+                return GetObject(effectPools, effectPrefabs, index);
+            default:
+                return null;
         }
     }
 
-
-    public GameObject Get(int index)
+    /// <summary>
+    /// 해당하는 풀에서 인덱스로 프리팹 목록에서 찾아 활성화.
+    /// </summary>
+    /// <param name="pools"></param>
+    /// <param name="prefabs"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public GameObject GetObject(List<GameObject>[] pools, GameObject[] prefabs, int index)
     {
         GameObject select = null;
 
